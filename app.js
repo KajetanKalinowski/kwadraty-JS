6a1141658e820a4adedb3e6bc02581a3a7e17e3e
KajetanKalinowski <kajtul999@gmail.com>
2024-04-26 12:31:03 +0200

    ?

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ var config = {
 startu.addEventListener("click",()=>{
     start(config.x,config.y,config.width,config.height)
     startu.style.display='none'
+    gora.style.display='block'
+    dol.style.display='block'
+    prawo.style.display='block'
+    lewo.style.display='block'
 })
 function start(){
 let kwadrat=rc.rectangle(config.x,config.y,config.width,config.height,{})
@@ -28,12 +32,12 @@ gora.addEventListener("click",()=>{
 })
 
 function drawGora(x,y,width,height){
-    y=config.height+config.gap
+    y=y-height-config.gap
     let kwadrat=rc.rectangle(x,y,width,height)
     x1=x+width/2
     y1=y+height
     x2=x+width/2
-    y2=y+height
+    y2=y+height+config.gap
 
     svg.appendChild(kwadrat)
     let line = rc.line(x1,y1,x2,y2); // x1, y1, x2, y2
@@ -42,4 +46,59 @@ function drawGora(x,y,width,height){
     console.log(x1,y1)
     console.log(x2,y2)
     console.log(x,y)
+    config.y=y
+}
+dol.addEventListener("click",()=>{
+    drawDol(config.x,config.y,config.width,config.height)
+})
+function drawDol(x,y,width,height){
+    y=height+config.gap+y
+    let kwadrat=rc.rectangle(x,y,width,height)
+    
+    x1=x+width/2
+    y1=config.y+height
+    x2=x+width/2
+    y2=config.y+height+config.gap
+    
+    svg.appendChild(kwadrat)
+    let line = rc.line(x1,y1,x2,y2); // x1, y1, x2, y2
+    svg.appendChild(line)
+    
+    config.y=y
+}
+prawo.addEventListener("click",()=>{
+    drawPrawo(config.x,config.y,config.width,config.height)
+})
+function drawPrawo(x,y,width,height){
+    x=width+config.gap+x
+    let kwadrat=rc.rectangle(x,y,width,height)
+    
+    x1=config.x+width
+    y1=y+height/2
+    x2=config.x+height+config.gap
+    y2=y+height/2
+    
+    svg.appendChild(kwadrat)
+    let line = rc.line(x1,y1,x2,y2); // x1, y1, x2, y2
+    svg.appendChild(line)
+    
+    config.x=x
+}
+lewo.addEventListener("click",()=>{
+    drawLewo(config.x,config.y,config.width,config.height)
+})
+function drawLewo(x,y,width,height){
+    x=config.x-width-config.gap
+    let kwadrat=rc.rectangle(x,y,width,height)
+    
+    x1=x+width+config.gap
+    y1=y+height/2
+    x2=x+width
+    y2=y+height/2
+    
+    svg.appendChild(kwadrat)
+    let line = rc.line(x1,y1,x2,y2); // x1, y1, x2, y2
+    svg.appendChild(line)
+    console.log(x,y)
+    config.x=x
 }
